eeda0ff67561b98639fc07627d400c0bfdfc48ad
Thomas Rueckstiess <thomas.rueckstiess@mongodb.com>
2021-08-10 12:12:24 +1000

    CLI can read from stdin

--- src/cli.js
+++ src/cli.js
@@ -2,21 +2,39 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import { redact } from "./redact";
 import { EJSON } from "bson";
+import readline from "readline";
 
 export function cli() {
-  const t = new Date();
-
-  const args = yargs(hideBin(process.argv))
+  let args = yargs(hideBin(process.argv))
     .example('$0 \'[{"$match": {"secretField": "secretValue"}}]\'')
     .option("salt", {
       alias: "s",
       type: "string",
       default: "",
       description: "Add custom salt to hash function",
-    })
-    .demandCommand(1).argv;
+    });
 
-  console.log(
-    EJSON.stringify(redact(EJSON.parse(args._[0]), { salt: args.salt }))
-  );
+  if (process.stdin.isTTY) {
+    args.demandCommand(1);
+  }
+  args = args.argv;
+
+  if (process.stdin.isTTY) {
+    console.log(
+      EJSON.stringify(redact(EJSON.parse(args._[0]), { salt: args.salt }))
+    );
+  } else {
+    // read from stdin line by line
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      terminal: false,
+    });
+
+    rl.on("line", function (line) {
+      console.log(
+        EJSON.stringify(redact(EJSON.parse(line), { salt: args.salt }))
+      );
+    });
+  }
 }
